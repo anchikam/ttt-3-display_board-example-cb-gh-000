1dc61ae8e52ee675ad8bd2aec44ddecd043f73e8
Anna Manukian <amanu001@odu.edu>
2019-04-01 19:46:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-row =  "   |   |   "
-line = "-----------"
-
 def display_board
+  row =  "   |   |   "
+  line = "-----------"
   puts row
   puts line
   puts row
